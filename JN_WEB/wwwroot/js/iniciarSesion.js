ff5d5ee4e582cdab4ed056ef24b9201ba2c13dd4
Jchavez60736 <jchavez60736@ufide.ac.cr>
2026-06-25 15:06:08 -0600

    validaciones frontend para iniciar sesion

---
+++ JN_WEB/wwwroot/js/iniciarSesion.js
@@ -0,0 +1,47 @@
+﻿$(function () {
+
+    $.validator.addMethod("caracterEspecial", function (value, element) {
+        return this.optional(element) || /[!@#$%^&*(),.?":{}|<>]/.test(value);
+    }, "");
+
+
+    $("#IniciarSesion").validate({
+        rules: {
+            CorreoElectronico: {
+                required: true,
+                email: true
+            },
+            Contrasenna: {
+                required: true,
+                minlength: 5,
+                caracterEspecial: true
+            }
+        },
+        messages: {
+            CorreoElectronico: {
+                required: "Campo obligatorio",
+                email: "Formato no válido"
+            },
+            Contrasenna: {
+                required: "Campo obligatorio",
+                minlength: "Mínimo 6 caracteres",
+                caracterEspecial: "Al menos 1 caracter especial"
+            }
+        },
+        errorElement: "span",
+        errorPlacement: function (error, element) {
+            error.addClass("text-danger small d-block");
+            element.closest(".form-group").after(error);
+        },
+        highlight: function (element) {
+            $(element).addClass("is-invalid");
+        },
+        unhighlight: function (element) {
+            $(element).removeClass("is-invalid").addClass("is-valid");
+        },
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+
+});
