09f84efc6f06f830f9d983f7ad6d0ca5ca7d3acd
Jchavez60736 <jchavez60736@ufide.ac.cr>
2026-07-22 15:13:28 -0600

    Cambiar Perfil

--- JN_WEB/Controllers/UsuarioController.cs
+++ JN_WEB/Controllers/UsuarioController.cs
@@ -14,12 +14,25 @@ namespace JN_WEB.Controllers
             return View();
         }
 
-        #region Cambiar Contraseña
+        #region Cambiar Contraseña y Perfil
 
         [HttpGet]
         public IActionResult Configuracion()
         {
-            return View();
+            var consecutivo = HttpContext.Session.GetInt32("Consecutivo")!.Value;
+
+            using var client = _http.CreateClient();
+            var url = _config["Valores:UrlApi"] + "Usuario/ConsultarUsuarioAPI?consecutivo=" + consecutivo;
+            var response = client.GetAsync(url).Result;
+
+            if (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.NotFound)
+            {
+                var datos = response.Content.ReadFromJsonAsync<UsuarioModel>().Result;
+
+                return View("Configuracion", datos);
+            }
+
+            throw new Exception("Error al cambiar la contraseña");
         }
 
         [HttpPost]
@@ -37,8 +50,28 @@ namespace JN_WEB.Controllers
             }
             else if (response.StatusCode == HttpStatusCode.BadRequest)
             {
-                ViewBag.Mensaje = response.Content.ReadAsStringAsync().Result;
-                return View();
+                ViewBag.MensajeSeguridad = response.Content.ReadAsStringAsync().Result;
+                return View("Configuracion", model);
+            }
+
+            throw new Exception("Error al cambiar la contraseña");
+        }
+
+        [HttpPost]
+        public IActionResult CambiarPerfil(UsuarioModel model)
+        {
+            model.Consecutivo = HttpContext.Session.GetInt32("Consecutivo")!.Value;
+
+            using var client = _http.CreateClient();
+            var url = _config["Valores:UrlApi"] + "Usuario/CambiarPerfilAPI";
+            var response = client.PutAsJsonAsync(url, model).Result;
+
+            if (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.BadRequest)
+            {
+                HttpContext.Session.SetString("Nombre", model!.Nombre);
+
+                ViewBag.MensajePerfil = response.Content.ReadAsStringAsync().Result;
+                return View("Configuracion", model);
             }
 
             throw new Exception("Error al cambiar la contraseña");
